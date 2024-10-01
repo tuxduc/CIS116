513ae14fd390ac889745c693e8825fda586e4bb9
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-01 16:23:32 -0400

    created script

--- 418LABExactchange.py
+++ 418LABExactchange.py
@@ -1,1 +1,21 @@
-print('Hello Evan!')
+totalChange= int(input())
+
+
+
+'''test1
+dollars = int(totalChange / 100)
+quarters = int(totalChange / 25)
+dimes = int(totalChange / 10)
+nickles = int(totalChange / 5)
+pennies = int(totalChange / 1)
+
+if dollars>0:
+    print(f'{dollars} Dollars')
+if quarters>0:
+    print(f'{quarters} Quarters')
+if dimes>0:
+    print(f'{dimes} Dimes')
+if nickles>0:
+    print(f'{nickles} Nickles')
+if pennies>0:
+    print(f'{pennies} Pennies')'''
