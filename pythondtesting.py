e922491d0b554d45a74f24f5274c4c1edbb24fb5
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-08 14:17:08 -0400

    null

--- pythondtesting.py
+++ pythondtesting.py
@@ -1,6 +1,9 @@
-target = int(input())
-n = int(input())
-step = 3
-while n >= target:
-    print(n * 2)
-    n -= step
+first_value = int(input())
+last_value = int(input())
+sum_vals = 0
+
+for x range(first_value, last_value + 1):
+    sum_vals += 1
+    print(x)
+
+print(f'Sequence sum: {sum_vals}')
