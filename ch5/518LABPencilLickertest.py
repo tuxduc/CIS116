5d257a7eec3cdb3e059460786c3cf2d9fa702e7a
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-15 15:57:10 -0400

    create

--- ch5/518LABPencilLickertest.py
+++
@@ -1,8 +0,0 @@
-def reverse_text():
-    while True:
-        user_input = input("Enter a line of text (or 'Done', 'done', or 'd' to exit): ")
-        if user_input in ["Done", "done", "d"]:
-            break
-        print(user_input[::-1])
-
-reverse_text()
