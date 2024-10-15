ae5b73115c88e7fd9c830aad3ed4504f9b41bb42
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-15 15:14:15 -0400

    experiment

---
+++ ch5/5.18LABPrintstringreverse.py
@@ -0,0 +1,18 @@
+'''
+Write a program that takes in a line of text as input, and outputs that line of text in reverse. The program repeats, ending when the user enters "Done", "done", or "d" for the line of text.
+
+Ex: If the input is:
+
+Hello there
+Hey
+done
+then the output is:
+
+ereht olleH
+yeH
+'''
+
+userTxt = input() [::-1]
+
+print(userTxt)
+
