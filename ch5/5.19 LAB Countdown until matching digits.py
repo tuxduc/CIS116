1c523fe2d13c26af41419db420c32f34fdd7078b
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-17 10:31:56 -0400

    changes

--- ch5/5.19 LAB Countdown until matching digits.py
+++ ch5/5.19 LAB Countdown until matching digits.py
@@ -1,1 +1,14 @@
 userInt = int(input())
+
+if 11 <= userInt <= 99:
+
+'''
+take the integer
+
+check and seee if the digits are the same
+    if the digits are same:
+        break
+    other wise if the digits are different;
+        add 1 to integer
+        print integer
+'''
