ea27f8a8a60aca09577197e89936e069d0faece3
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-17 10:54:51 -0400

    chg

--- ch5/5.19 LAB Countdown until matching digits.py
+++ ch5/5.19 LAB Countdown until matching digits.py
@@ -1,7 +1,16 @@
 userInt = int(input())
+intToStr = str(userInt)
+#if 11 <= userInt <= 99:
 
-if 11 <= userInt <= 99:
+# print(intToStr[0]) we can index the str
 
+#for x in range(len(intToStr)):
+while 11 <= userInt <= 99:
+    if intToStr[0] == intToStr[1]:
+        exit()
+    else:
+        userInt += 1
+        print(userInt)
 '''
 take the integer
 
