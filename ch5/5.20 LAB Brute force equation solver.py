261358df57335638a432a91489905e435edde377
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-19 11:16:50 -0400

    chg

--- ch5/5.20 LAB Brute force equation solver.py
+++ ch5/5.20 LAB Brute force equation solver.py
@@ -1,28 +1,24 @@
 #Read in first equation, ax + by = c '''
-a = int(input())
-b = int(input())
-c = int(input())
+a = 8 #int(input())
+b = 7 #int(input())
+c = 38 #int(input())
 
 #Read in second equation, dx + ey = f '''
-d = int(input())
-e = int(input())
-f = int(input())
+d = 3 #int(input())
+e = -5 #int(input())
+f = -1 #int(input())
 
 #''' Type your code here. '''
-eq1 = ax + by = c
-eq2 = dx + ey = f
-
-
-for x in range(-10,10):
-   for y in range(-10,10):
-       if eq1 == 38 
-        print(f'x= {x} , y = {y}')
-else:
-    print('There is no solution')
+x= 0
+y= 0
+#eq1 = a*x + b*y == c
+#eq2 = d*x + e*y == f
 
 for x in range(-10,10):
-   for y in range(-10,10):
-       if eq1 == 38 and eq2 == -1:
-        print(f'x= {x} , y = {y}')
-else:
+    for y in range(-10,10):
+        if a*x + b*y == c and d*x + e*y:
+         check = True
+         print(f'x= {x} , y = {y}')
+
+if check == False:
     print('There is no solution')
