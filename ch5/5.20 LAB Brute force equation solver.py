5d9a2fe72986830d863af4360152afcd0bf3df69
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-19 11:02:10 -0400

    chg

---
+++ ch5/5.20 LAB Brute force equation solver.py
@@ -0,0 +1,28 @@
+#Read in first equation, ax + by = c '''
+a = int(input())
+b = int(input())
+c = int(input())
+
+#Read in second equation, dx + ey = f '''
+d = int(input())
+e = int(input())
+f = int(input())
+
+#''' Type your code here. '''
+eq1 = ax + by = c
+eq2 = dx + ey = f
+
+
+for x in range(-10,10):
+   for y in range(-10,10):
+       if eq1 == 38 
+        print(f'x= {x} , y = {y}')
+else:
+    print('There is no solution')
+
+for x in range(-10,10):
+   for y in range(-10,10):
+       if eq1 == 38 and eq2 == -1:
+        print(f'x= {x} , y = {y}')
+else:
+    print('There is no solution')
