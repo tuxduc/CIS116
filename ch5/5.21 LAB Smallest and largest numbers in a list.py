df77112476ff5bae14b7517fe937d2a31b9de63b
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-22 15:54:23 -0400

    chg

--- ch5/5.21 LAB Smallest and largest numbers in a list.py
+++ ch5/5.21 LAB Smallest and largest numbers in a list.py
@@ -1,4 +1,5 @@
-numList =[int(input()), int(input()), int(input()), int(input()), int(input()), int(input())]
+#numList =[int(input()), int(input()), int(input()), int(input()), int(input()), int(input())]
+numList = []
 max= 0
 min = 100
 
@@ -8,6 +9,14 @@ min = 100
 #   if x < min
 #       min = x
 # print(f'{min} and {max}')
+userinput = int(input())
+
+while userinput != 0:
+        numList.append(userinput)
+else:
+    break
+
+
 for x in numList:
    if x > max:
        max = x
--- test2.py
+++ test2.py
@@ -1,9 +1,44 @@
-''' Read in first equation, ax + by = c '''
+def number_of_pennies(d,p=0):
+    dollTOpen = d / 100
+    total_pennies = dollTOpen + p
+    return total_pennies
+
 a = int(input())
 b = int(input())
 c = int(input())
 
-''' Read in second equation, dx + ey = f '''
+number_of_pennies(a,b)
+number_of_pennies(c)
+
+print(number_of_pennies(int(input()), int(input()))) # Both dollars and pennies
+print(number_of_pennies(int(input())))               # Dollars only
+
+'''
+def mph_and_minutes_to_miles(x,y):
+    hours_traveled = minutes_traveled / 60.0
+    miles_traveled = hours_traveled * miles_per_hour
+
+miles_per_hour = float(input())
+minutes_traveled = float(input())
+
+print(f'Miles: {mph_and_minutes_to_miles(miles_per_hour, minutes_traveled):f}')
+
+
+            #start of txt repository
+def get_minutes_as_hours(orig_minutes):
+    minutes = orig_minutes/60
+    return minutes
+
+minutes = float(input())
+print(get_minutes_as_hours(minutes))
+
+
+#Read in first equation, ax + by = c
+a = int(input())
+b = int(input())
+c = int(input())
+
+#Read in second equation, dx + ey = f 
 d = int(input())
 e = int(input())
 f = int(input())
@@ -21,3 +56,6 @@ if check == False:
 
 # a * x + b * y = c
 # d * x + e * y = f
+        
+            #end of text reopsitory
+'''
