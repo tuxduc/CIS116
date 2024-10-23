05fe2447579b5df87cc23c9aca3ceaf81905e380
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-23 15:52:44 -0400

    chg

--- ch6/6.18 LAB Track laps to miles.py
+++ ch6/6.18 LAB Track laps to miles.py
@@ -1,9 +1,8 @@
 # Define your function here 
-
-if __name__ == '__main__':
-    # Type your code here. Your code must call the function. 
 def laps_to_miles(num_laps):
     miles = num_laps * .25 
     return miles
+if __name__ == '__main__':
+    # Type your code here. Your code must call the function. 
 
-print(f'{laps_to_miles(float(input())):.2f}')
+    print(f'{laps_to_miles(float(input())):.2f}')
