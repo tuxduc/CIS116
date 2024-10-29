71eda4d72c4e86f445ab236356da4c5f68ed953d
Andrew Anderson <anderson.andrew632@gmail.com>
2024-10-29 14:28:38 -0400

    chg

---
+++ ch7/7.9LABpalindrome.py
@@ -0,0 +1,10 @@
+userWord = input()
+userWordReversed = 
+
+
+"""
+if user word reveresed is same as user word, then:
+    word is a palindrome
+otherwise:
+    not a palindrome
+"""
--- test2.py
+++ test2.py
@@ -1,2 +1,6 @@
-print(17/2)
-print(17%2)
+def my_function(x):
+  return x[::-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
